171c65d218ce33252d51fa44e80bd99fa4f85924
MrKelada1 <danieler.main@gmail.com>
2024-11-06 15:52:33 +0200

    Changed method to card centric approach

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-from smartcard.System import readers
+import smartcard.Exceptions
+from smartcard.CardType import AnyCardType
+from smartcard.CardRequest import CardRequest
 from smartcard.util import toHexString
 
-# List all available readers
-available_readers = readers()
-if not available_readers:
-    print("No smart card readers found.")
+cardtype = AnyCardType()
+
+cardrequest = CardRequest(timeout=1, cardType=cardtype)
+
+try:
+    cardservice = cardrequest.waitforcard()
+except smartcard.Exceptions.CardRequestTimeoutException:
+    print("no card detected!")
 else:
-    reader = available_readers[0]
-    print("Using reader:", reader)
-
-    # Connect to the reader
-    connection = reader.createConnection()
-    connection.connect()
-
-    # Example: sending a command to get card info (this depends on card type)
-    get_data_command = [0x00, 0xA4, 0x04, 0x00, 0x00]  # Replace with actual command for your card
-    data, sw1, sw2 = connection.transmit(get_data_command)
-
-    # Print the response (card data)
-    print("Card data:", toHexString(data))
-    print("Status words:", hex(sw1), hex(sw2))
+    cardservice.connection.connect()
+    print("card's ATR is:" + toHexString(cardservice.connection.getATR()))
